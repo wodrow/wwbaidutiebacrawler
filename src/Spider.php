1f61f3754daa86913f6d2211ab83e069a95e8a5e
wodrow-tigo120-tc <wodrow451611cv@gmail.com>
2020-02-01 21:47:40 +0800

    init

--- src/Spider.php
+++ src/Spider.php
@@ -9,7 +9,198 @@
 namespace wodrow\wwbaidutiebacrawler;
 
 
-class Spider
-{
+use QL\QueryList;
+use wodrow\yii2wtools\tools\FileHelper;
+use yii\base\Component;
+use yii\base\Exception;
+use yii\helpers\Html;
 
+class Spider extends Component
+{
+    public $url;
+    public $is_console = 0;
+
+    /**
+     * @param $msg
+     * @throws
+     */
+    public function error($msg)
+    {
+        if ($this->is_console){
+            var_dump($msg);
+        }else{
+            throw new Exception($msg);
+        }
+    }
+
+    /**
+     * @throws
+     */
+    public function checkIsTieZi()
+    {
+        $_url = $this->url;
+        $_url = str_replace('https', 'http', $_url);
+        $str = "http://tieba.baidu.com/p/";
+        $_id = str_replace($str, '', $_url);
+        if (strpos($_id, '?')!==false){
+            $arr = explode('?', $_id);
+            $_id = $arr[0];
+        }
+        if (is_numeric($_id)) {
+            $this->url = $str.$_id;
+        } else {
+            $this->error('不是帖子');
+        }
+    }
+
+    public function getList()
+    {
+        $ql = QueryList::getInstance()->get($this->url);
+        $this->title = $ql->find(".core_title_txt")->attr('title');
+        $this->title .= " [引自百度贴吧]";
+        $this->post_ids = [];
+        $pages = $ql->find('.l_reply_num')->find('.red:eq(1)')->text();
+        if ($this->is_console)var_dump($pages);
+        if (!$this->is_cache)\Yii::$app->cache->delete('FormTieBaiduSpider-generate');
+        $list = \Yii::$app->cache->get('FormTieBaiduSpider-generate');
+        if (!$list){
+            $list = [];
+            for ($i = 1; $i <= $pages; $i++){
+                if ($this->is_console)var_dump($i);
+                if ($i == 1){
+                    $_ql = $ql;
+                }else{
+                    $_ql = QueryList::getInstance()->get($this->url."?pn={$i}");
+                }
+                $list = ArrayHelper::merge($list, $_ql->rules([
+//                    'html' => ['.j_l_post:visible .j_d_post_content', 'html'],
+                    'html' => ['.j_l_post:visible', 'html'],
+                    'text' => ['.j_l_post:visible', 'text'],
+                    'tail' => ['.j_l_post:visible', 'data-field'],
+                ])->queryData());
+            }
+            \Yii::$app->cache->set('FormTieBaiduSpider-generate', $list, 3600);
+        }
+        foreach ($list as $k => $v){
+            $_ql = QueryList::getInstance()->html($v['html']);
+            if (!$_ql->find('.j_d_post_content')->html()){
+                unset($list[$k]);
+            }else{
+                $list[$k]['html'] = $_ql->find('.j_d_post_content')->html();
+                $list[$k]['text'] = $_ql->find('.j_d_post_content')->text();
+                $list[$k]['tail'] = json_decode($v['tail'], true);
+            }
+        }
+        if (count($list) > 0){
+            /*foreach ($list as $k => $v) {
+                if ($this->is_console)var_dump("megre:".$k);
+                if (isset($v['tail'])){
+                    $tail = json_decode($list[$k]['tail'], true);
+                    $fn = function (&$list, $k)use(&$fn, $tail){
+                        if (!isset($list[$k]['html'])){
+                            unset($list[$k]);
+                            $k--;
+                            $fn($list, $k);
+                        }else{
+                            $list[$k]['tail'] = $tail;
+                        }
+                    };
+                    $fn($list, $k);
+                }
+            }*/
+            foreach ($list as $k => $v) {
+                if ($this->is_console)var_dump("list:".$k);
+                $_ql = QueryList::getInstance()->html($v['html']);
+                $images = $_ql->rules([
+                    'image' => ['img', 'src'],
+                ])->queryData();
+                $videos = $_ql->rules([
+                    'video' => ['embed', 'data-video'],
+                ])->queryData();
+                $imgs = $this->saveTieBaImage($images);
+                $vids = $this->saveTieBaVideo($videos);
+                $list[$k]['text'] = "<p>".implode('', $vids)."</p>"."<p>{$v['text']}</p>"."<p>".implode('', $imgs)."</p>";
+//                $list[$k]['images'] = $imgs;
+//                $list[$k]['videos'] = $vids;
+            }
+            foreach ($list as $k => $v){
+                if (isset($v['tail'])) {
+                    $tail = $list[$k]['tail'];
+                    $this->post_ids[] = $tail['content']['post_id'];
+                }
+            }
+            $this->author_id = $list[0]['tail']['author']['user_id'];
+            $this->author_name = $list[0]['tail']['author']['user_name'];
+        }
+        return $list;
+    }
+
+    /**
+     * @param $images
+     * @return array
+     * @throws
+     */
+    public function saveTieBaImage($images)
+    {
+        $_dir = str_replace("http://tieba.baidu.com/p/", "", $this->url);
+        $upload_root = \Yii::getAlias("@uploads_root/baidu_tieba/{$_dir}");
+        $upload_url = \Yii::getAlias("@uploads_url/baidu_tieba/{$_dir}");
+        if (!is_dir($upload_root)){
+            FileHelper::createDirectory($upload_root);
+        }
+        $imgs = [];
+        foreach ($images as $k => $v){
+            $image_name = basename($v['image']);
+            if (strpos($image_name, '.png?t=') !== false){
+                $_x = explode("?t=", $image_name);
+                $image_name = $_x[0];
+            }
+            $root = $upload_root.DIRECTORY_SEPARATOR.$image_name;
+            $url = $upload_url.DIRECTORY_SEPARATOR.$image_name;
+            if (!file_exists($root)){
+                $fg_con = @file_get_contents($v['image']);
+                if ($fg_con){
+                    file_put_contents($root, $fg_con);
+                    $imgs[] = Html::img($url, ['class' => "img img-responsive"]);
+                    if ($this->is_console)var_dump($url);
+                }
+            }
+        }
+        return $imgs;
+    }
+
+    /**
+     * @param $videos
+     * @return array
+     * @throws
+     */
+    public function saveTieBaVideo($videos)
+    {
+        $_dir = str_replace("http://tieba.baidu.com/p/", "", $this->url);
+        $upload_root = \Yii::getAlias("@uploads_root/baidu_tieba/{$_dir}");
+        $upload_url = \Yii::getAlias("@uploads_url/baidu_tieba/{$_dir}");
+        if (!is_dir($upload_root)){
+            FileHelper::createDirectory($upload_root);
+        }
+        $vids = [];
+        foreach ($videos as $k => $v){
+            $video_name = basename($v['video']);
+            if (strpos($video_name, '.png?t=') !== false){
+                $_x = explode("?t=", $video_name);
+                $video_name = $_x[0];
+            }
+            $root = $upload_root.DIRECTORY_SEPARATOR.$video_name;
+            $url = $upload_url.DIRECTORY_SEPARATOR.$video_name;
+            if (!file_exists($root)){
+                $fg_con = @file_get_contents($v['image']);
+                if ($fg_con){
+                    file_put_contents($root, $fg_con);
+                    $vids[] = "<video src='{$url}' controls='controls'>您的浏览器不支持 video 标签</video>";
+                }
+            }
+//            $fi = new \finfo(FILEINFO_MIME_TYPE);
+//            $mime_type = $fi->file($root);
+        }
+        return $vids;
+    }
 }
